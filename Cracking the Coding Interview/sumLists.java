8d622e1b134e9accf12304cce36ef6bda04280e6
Richard Ho <richardho94@gmail.com>
2015-11-23 10:57:55 -0600

    sumLists

---
+++ Cracking the Coding Interview/sumLists.java
@@ -0,0 +1,51 @@
+private Node sumLists(Node one, Node two) {
+  if(one == null || two == null) {
+    if(one == null) {
+      return two;
+    }
+    else if(two == null) {
+      return one;
+    }
+  }
+
+  int sum = one.data + two.data;
+  boolean carry = false
+  if(sum > 9) {
+    sum %= 10;
+    carry = true;
+  }
+  Node head = new Node(sum);
+  Node current = head;
+  while(one != null || two != null) {
+    Node newNode = null;
+    sum = 0;
+    if(one != null) {
+      sum += one.data;
+    }
+    if(two != null) {
+      sum += two.data
+    }
+    if(carry) {
+      sum += 1;
+    }
+    if(sum > 9) {
+      sum %= 10;
+      carry = true;
+    }
+    else {
+      carry = false;
+    }
+    newNode = new Node(sum);
+    current.next = newNode;
+    current = newNode;
+
+    if(one != null) {
+      one = one.next;
+    }
+
+    if(two != null) {
+      two = two.next;
+    }
+  }
+  return head;
+}
